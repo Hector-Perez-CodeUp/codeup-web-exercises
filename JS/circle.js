d561a0f2549bb25a4d0bd76230c9351013a0c507
Hector Perez <hector.elliott.perez@gmail.com>
2022-09-21 15:02:47 -0700

    completed Math circle exercises

--- JS/circle.js
+++ JS/circle.js
@@ -11,23 +11,22 @@
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+            if (doRounding == true) {
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+                 return Math.round(circle.getArea());
+             }
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+             return circle.getArea();
         }
     };
-
-    // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
 
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
---
+++ JS/practice.js
@@ -0,0 +1,6 @@
+
+// radius: 3,
+//     getArea: function () {
+//     return Math.PI * (Math.pow(this.radius, 2));
+console.log("Area of a circle with R: 3 = " + Math.PI * (Math.pow(3, 2)))
+console.log("Area of a circle with R: 5 = " + Math.PI * (Math.pow(5, 2)))
