debe706c5e7dcb21831f50808e81409eb6652130
Hector Perez <hector.elliott.perez@gmail.com>
2022-09-21 09:00:31 -0700

    warmup up to shuffle an array

--- JS/practice.js
+++ JS/practice.js
@@ -1,14 +1,42 @@
-function createBook() {
-    let bookTitles = ["Breathe", "Preacher", "1984", "Extreme Ownership", "The Art of War"];
-    let bookAuthors = ["Rickson Gracie", "Garth Ennis", "George Orwell", "Jocko Willink", "Sun Tzu"]
-    let library = [];
-    let newBook = {};
-    for (let i = 0; i < bookTitles.length; i++) {
-        newBook["Number"] = (i + 1);
-        newBook["title"] = (bookTitles[i]);
-        newBook["author"] = (bookAuthors[i]);
-        library.push(newBook)
-    }
-    console.log(library)
+// Area of a Triangle
+function areaOfTriangle(a, b, c) {
+    let s = (a + b + c); // semi-perimeter
+    return Math.sqrt(s(s-a)(s-b)(s-c));
 }
-createBook();
+
+
+// Reverse a String
+function revString(str) {
+    let strSplit = str.split("");
+    let strReverse = strSplit.reverse();
+    let joinArray = strSplit.join("")
+    console.log(joinArray)
+}
+//revString("codeup")
+
+
+// FizzBuzz Activity
+for ( let i = 0; i <= 100; i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i + " 'FizzBuzz'");
+        continue;
+    } else if (i % 5 == 0) {
+        console.log(i + " 'Pop'");
+        continue;
+    } else if (i % 3 == 0) {
+        console.log(i + " 'Fizz'");
+        continue;
+    }
+    console.log(i);
+}
+
+//
+// if (i % 3 == 0) {
+//     console.log(i + " 'Fizz'");
+//     continue;
+// } else if (i % 5 == 0) {
+//     console.log(i + " 'Pop'");
+//     continue;
+// }
+// console.log(i);
+// }
