7cefbdd9e415852e0d2e81a784a901dde37dd509
Hector Perez <hector.elliott.perez@gmail.com>
2022-09-21 12:54:09 -0700

    completed 9/21 warmups

--- JS/warmup_9_21.js
+++ JS/warmup_9_21.js
@@ -31,12 +31,25 @@ for ( let i = 0; i <= 100; i++){
 }
 
 //
-// if (i % 3 == 0) {
-//     console.log(i + " 'Fizz'");
-//     continue;
-// } else if (i % 5 == 0) {
-//     console.log(i + " 'Pop'");
-//     continue;
+// // Shuffle an Array
+// function shuffleArray(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let randomIndex = Math.floor(Math.random() * (arr.length + 1));
+//         let randomElement = arr.splice( , 1)
+//         console.log(arr)
+//     }
 // }
-// console.log(i);
-// }
+// //shuffleArray([1, 2, 3, 4, 5])
+
+//console.log(arr);
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
