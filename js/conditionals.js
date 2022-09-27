5e5c52ad78bb3f063ba509341ffe2d014a917410
Hector Perez <hector.elliott.perez@gmail.com>
2022-09-27 13:38:33 -0700

    refactored conditionals.js

--- js/conditionals.js
+++ js/conditionals.js
@@ -3,94 +3,87 @@
 //analyzeColor Function
 function analyzeColor(color) {
     if (color == 'blue') {
-        alert("blue is the color of the sky");
+        return("blue is the color of the sky");
     } else if ( color == 'red') {
-        alert('strawberries are red');
+        return('strawberries are red');
     } else if ( color == 'orange') {
-        alert('oranges are orange');
+        return('oranges are orange');
     } else if ( color == 'yellow') {
-        alert('bananas are yellow');
+        return('bananas are yellow');
     } else if ( color == 'green') {
-        alert('kale is green');
+        return('kale is green');
     } else {
-        alert("I don't know anything about " + color );
+        return("I don't know anything about " + color );
     }
 }
-analyzeColor("blue")
+console.log(analyzeColor("cyan"));
 
 
 //randomColor Function
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 var randomColor = colors[Math.floor(Math.random() * colors.length)];
-//analyzeColor(randomColor);
+console.log(analyzeColor(randomColor));
 
 
 //analyzeColor Switch Case
 switch(randomColor) {
     case "blue": {
-        alert("blue is the color of the sky");
+        console.log("blue is the color of the sky");
         break; }
     case "red": {
-        alert("strawberries are red");
+        console.log("strawberries are red");
         break; }
     case "orange": {
-        alert("oranges are orange");
+        console.log("oranges are orange");
         break; }
     case "yellow": {
-        alert("bananas are yellow");
+        console.log("bananas are yellow");
         break;}
     case "green": {
-        alert("kale is green");
+        console.log("kale is green");
         break; }
     default:
-        alert("I don't know anything about " + randomColor);
+        console.log("I don't know anything about " + randomColor);
         break; }
 
 
-// // User Input
-let favColor = prompt("Enter your favorite color");
-analyzeColor(favColor);
+// User Input
+//let favColor = prompt("Enter your favorite color");
+//alert(analyzeColor(favColor));
 
 
 // calculateTotal Function
 function calculateTotal(luckyNum, total) {
     if ( luckyNum == 0 ) {
-        alert("Your total is " + total);
+        return total;
     } else if ( luckyNum == 1 ) {
-        let discountPercent = 0.1;
-        total = total - ( total * discountPercent);
-        alert("Your new total is now:  $" + total);
+        return total - (total * 0.1);
     } else if ( luckyNum == 2 ) {
-        let discountPercent = 0.25;
-        total = total - ( total * discountPercent);
-        alert("Your new total is now:  $" + total);
+        return total - (total * .25);
     } else if ( luckyNum == 3 ) {
-        let discountPercent = 0.35;
-        total = total - ( total * discountPercent);
-        alert("Your new total is now:  $" + total);
+        return total - (total * .35);
     } else if ( luckyNum == 4 ) {
-        let discountPercent = 0.5;
-        total = total - ( total * discountPercent);
-        alert("Your new total is now:  $" + total);
+        return total - (total * 0.5);
     } else if ( luckyNum == 5 ) {
-        alert("Your item is now free!");
+        return 0;
     }
 }
-calculateTotal(5, 100);
+console.log(calculateTotal(5, 100));
 
 
 // Random Lucky Number Input
 var luckyNumber = Math.floor(Math.random() * 6);
 let totalBill = prompt("Enter your bill total");
 alert("Your random lucky number is " + luckyNumber);
-calculateTotal(luckyNumber, totalBill);
+alert("Your price before the discount was: $" + totalBill);
+alert("Your price after the discount is: $" + calculateTotal(luckyNumber, totalBill));
 
 
 // enterNum Function
 let enterNum = confirm("Would you like to enter a number?");
 if ( enterNum == true ) {
     let number = Number(prompt("Please enter your number"));
-    if ( isNaN(number) ) {
+    if (isNaN(number)) {
         alert("Incorrect input data type.");
     } else {
         let positiveNegative = number >= 0 ? "Your number is positive" : "Your number is negative.";
