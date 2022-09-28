2e8f4eb4eb5cf1e0b4f491dbcf03e6ed4a767308
Hector Perez <hector.elliott.perez@gmail.com>
2022-09-28 13:48:48 -0700

    refactored planets-strings.js

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,10 +1,53 @@
 (function(){
     "use strict";
 
+    var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
     let planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
     let planetsArray = planetsString.split('|');
     console.log(planetsArray);
 
+
+    // Add "The Sun" to beginning of Planets
+    planets.unshift("The Sun");
+    console.log("Adding 'The Sun' to the beginning of the planets array.");
+    console.log(planets);
+
+
+    // Add "Pluto" to the end of Planets
+    planets.push("Pluto");
+    console.log("Adding 'Pluto' to the end of the planets array.");
+    console.log(planets);
+
+
+    // Removing "The Sun" from Planets
+    planets.shift();
+    console.log("Removing 'The Sun' from the planets array.");
+    console.log(planets);
+
+
+    // Removing "Pluto" from Planets
+    planets.pop();
+    console.log("Removing 'Pluto' from the end of the planets array.");
+    console.log(planets);
+
+
+    // Index of "Earth" in Planets
+    console.log("Finding and logging the index of 'Earth' in the planets array.");
+    console.log(planets.indexOf("Earth"));
+
+
+    // Reverse Planets Array
+    planets.reverse();
+    console.log("Reversing the order of the planets array.");
+    console.log(planets);
+
+
+    // Sorting Planets
+    planets.sort();
+    console.log("Sorting the planets array.");
+    console.log(planets);
+
+    //
     let brString = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
     console.log(brString);
 
--- js/planets-strings.js
+++ js/planets-strings.js
@@ -1,15 +1,18 @@
 (function(){
     "use strict";
 
+    // Convert Planets string to array
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
     var planetsArray = planetsString.split("|");
     console.log(planetsArray);
 
 
+    // Added <br> tags between each planet
     let brPlanetsString = planetsString.replaceAll("|", "<br>");
     console.log(brPlanetsString);
 
 
+    // Unordered List
     let liPlanetsString = planetsString.replaceAll("|", "</li><li>");
     console.log("<ul><li>" + liPlanetsString + "</li></ul>");
 })();
