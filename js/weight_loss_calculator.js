af5ac63c84c62105e72d6a4049e05872961fa6fd
Hector Perez <hector.elliott.perez@gmail.com>
2022-10-15 09:46:44 -0700

    need to add dom button functionality

--- js/weight_loss_calculator.js
+++ js/weight_loss_calculator.js
@@ -1,28 +1,3 @@
-alert("Welcome to the Weight Loss Calculator!");
-
-// Subject Data Gathering
-let age = prompt("Enter your age:  ");
-let c_weight = prompt("Enter your current weight in lbs: ");
-let g_weight = prompt("Enter your goal weight in lbs:  ");
-let weight_deficit = c_weight - g_weight;
-let time_to_goal = weight_deficit / 2; // rate of 2lb lbs per week
-let height = prompt("Enter your height in cm:  ");
-let sex = prompt("Enter M for Male or F for Female:  ");
-let weight_kg = c_weight * .453592;  // lbs to kg
-let bmr = ((10 * weight_kg) + (6.25 * height) - (5 * age));
-let bmr_male = (bmr + 5);
-let bmr_female = (bmr - 161);
-
-
-// Present Activity Levels
-act_lvl = prompt("Input the number associated with your current activity level:\n"
-    + "1.  Sedentary (little to no exercise + desk job)\n"
-    + "2.  Lightly Active (light exercise 1-3 days a week)\n"
-    + "3.  Moderately Active (moderate exercise 3-5 days a week\n"
-    + "4.  Very Active (heavy exercise 6-7 days a week)\n"
-    + "5.  Extremely Active (strenuous training 2x a day)"
-);
-
 
 // Final Calculations
 let onePound = "Losing 1 lb per week, you should reach your goal in ";
@@ -72,5 +47,3 @@ if ( sex == "F" && act_lvl == 1 ) {
     alert(onePound + weight_deficit + " weeks!\n" + maintain + Math.floor((tdee - 500)) + " a day. \n" +
         twoPounds + time_to_goal + " weeks!\n" + maintain + Math.floor((tdee - 1000)) + " a day.");
 }
-
-alert("Good luck!");
