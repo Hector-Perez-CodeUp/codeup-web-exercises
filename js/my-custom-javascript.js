2897ad6e3d7f87dd51297bf144a971041153cb32
Hector Perez <hector.elliott.perez@gmail.com>
2022-10-19 12:36:26 -0700

    completed jquery selectors exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,29 @@
-window.onload = function() {
-    alert('windowon.load');
-}
+// ID Selectors Exercises
+// let subtitle = $("#subtitle").html();
+// alert(subtitle);
+//
+$(".poster").css("height", "200px");
+//
+// window.onload = function() {
+//     alert('windowon.load');
+// }
+//
+// $(function() {
+//     alert("The DOM has finished loading")
+// });
 
-$(function() {
-    alert("The DOM has finished loading")
-});
 
+//Class Selectors
+// $(".codeup").css("border", "1px solid red");
+
+
+//Element Selectors
+// $(".list-items").css("font-size", "20px");
+// $("h1").css("background-color", "orange");
+// $("p").css("background-color", "orange");
+// $("li").css("background-color", "orange");
+// let title = $("#title").html();
+// alert(title);
+
+// Multiple Selectors
+$("h1, p, li").css("background-color", "red");
