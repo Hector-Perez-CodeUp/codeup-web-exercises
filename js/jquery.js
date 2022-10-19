7e91210aceb76cd4d48e1d09f493acebbe952bd1
Hector Perez <hector.elliott.perez@gmail.com>
2022-10-19 14:28:42 -0700

    refactored konami code

--- js/jquery.js
+++ js/jquery.js
@@ -18,12 +18,14 @@ $(document).keyup(function(event) {
 function checkCode() {
     if (JSON.stringify(inputArray) === JSON.stringify(konamiCode)) {
         alert("You have added 30 lives!");
-        $("html").css("background-image", "url(img/eight.jpeg)");
-        $("html").css("-webkit-background-size", "cover");
-        $("html").css("-moz-background-size", "cover");
-        $("html").css("-0-background-size", "cover");
-        $("html").css("background-size", "cover");
-        $("h1").css("background-color", "purple");
+        $("html").css({
+            "background-image": "url(img/eight.jpeg)",
+            "-webkit-background-size": "cover",
+            "-moz-background-size": "cover",
+            "-0-background-size": "cover",
+            "background-size": "cover",
+            "background-color": "purple",
+        });
         $("h1").css("color", "white");
         const successSound = new Audio("audio/511484__mlaudio__success-bell.wav");
         successSound.play();
