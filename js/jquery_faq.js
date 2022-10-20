912655767c8912c821b2cbbb7a2484de31306a16
Hector Perez <hector.elliott.perez@gmail.com>
2022-10-20 15:25:53 -0700

    completed jquery_faq bonus exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -21,3 +21,48 @@ $("h3").click(function() {
 $("li").click(function() {
     $(this).parent().children().first().next().css("color", "blue");
 })
+
+//Bonus
+$(".frame").css({
+    "border": "10px black",
+    "border-style": "outset",
+    "display": "inline-block",
+    "height": "210px",
+    "width": "310px"
+});
+
+$("img").css({
+    "height": "200px",
+    "width": "300px"
+});
+
+$("button").css("margin-left", "200px");
+
+$("#left-btn").click(function() {
+    let img1 = $("#left-img").attr('src');
+    let img2 = $("#center-img").attr('src');
+    $("#left-img").attr("src", img2);
+    $("#center-img").attr("src", img1);
+})
+
+$("#right-btn").click(function() {
+    let img2 = $("#center-img").attr('src');
+    let img3 = $("#right-img").attr('src');
+    $("#right-img").attr("src", img2);
+    $("#center-img").attr("src", img3);
+})
+
+$("#center-btn").click(function() {
+    let oneOrZero = Math.round(Math.random())
+    if (oneOrZero === 0) {
+        let img1 = $("#left-img").attr('src');
+        let img2 = $("#center-img").attr('src');
+        $("#left-img").attr("src", img2);
+        $("#center-img").attr("src", img1);
+    } else {
+        let img2 = $("#center-img").attr('src');
+        let img3 = $("#right-img").attr('src');
+        $("#right-img").attr("src", img2);
+        $("#center-img").attr("src", img3);
+    }
+})
