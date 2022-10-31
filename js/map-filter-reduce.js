1258a4aef2220208489fab60deb90810ab08c13d
Hector Perez <hector.elliott.perez@gmail.com>
2022-10-31 12:40:20 -0700

    completed languages filter

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,58 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+
+// 3 Language Users Using .filter
+var threeLanguages = users.filter(function(person) {
+    return person.languages.length >= 3;
+})
+console.log(threeLanguages)
+
+// User Emails Using .map
+
+
+// Total Years of Exp using .reduce
+
+
+// Longest Email Using .reduce
+
+
+// All User Names Using .reduce
+
+
+// Bonus:  Unique List of Languages Using .reduce
